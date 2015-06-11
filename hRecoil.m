891a5ac852f6283f4c211001b6f5021a64677fea
Kalyan12 <Tarcall00@gmail.cmo>
2015-06-11 11:13:44 -0400

    Create hRecoil.m
    Horizontal recoil function. This is where most of the speculation comes in...

---
+++ hRecoil.m
@@ -0,0 +1,53 @@
+function [xFinal, yFinal] = hRecoil(averageAngle, recoilAngle, Max, Min, xOne, yOne, Tolerance)
+
+    % Chooses direction: Left = -1 Right = 1
+    if (randi(2) == 1)
+        Direction = 1;
+    else
+        Direction = -1;
+    end
+    
+    % Calculating horizontal shake
+    recoilAmount = (Max - Min) * rand + Min;
+
+    % Making sure that recoil goes the correct direction
+    if (recoilAngle >= 0 && Direction == 1)
+        xFinal = xOne + cosd(recoilAngle) * recoilAmount;
+        yFinal = yOne - sind(recoilAngle) * recoilAmount;
+    elseif (recoilAngle >= 0 && Direction == -1)
+        xFinal = xOne - cosd(recoilAngle) * recoilAmount;
+        yFinal = yOne + sind(recoilAngle) * recoilAmount;
+    elseif (recoilAngle <0 && Direction == 1)
+        xFinal = xOne + cosd(-recoilAngle) * recoilAmount;
+        yFinal = yOne + sind(-recoilAngle) * recoilAmount;
+    else
+        xFinal = xOne - cosd(-recoilAngle) * recoilAmount;
+        yFinal = yOne - sind(-recoilAngle) * recoilAmount;
+    end
+   
+    % Changes direction of recoil if larger than tolerance
+    xMax = (yFinal/tand(90-averageAngle)) + (Tolerance/2);
+    xMin = (yFinal/tand(90-averageAngle)) - (Tolerance/2);
+    
+    
+    if (xFinal > xMax)
+        if (recoilAngle >= 0)
+            xFinal = xOne - cosd(recoilAngle) * recoilAmount;
+            yFinal = yOne + sind(recoilAngle) * recoilAmount;
+        elseif (recoilAngle <0)
+            xFinal = xOne - cosd(-recoilAngle) * recoilAmount;
+            yFinal = yOne - sind(-recoilAngle) * recoilAmount;
+        end
+    end
+    if (xFinal < xMin)
+        if (recoilAngle >= 0)
+            xFinal = xOne + cosd(recoilAngle) * recoilAmount;
+            yFinal = yOne - sind(recoilAngle) * recoilAmount;
+        elseif (recoilAngle <0)
+            xFinal = xOne + cosd(-recoilAngle) * recoilAmount;
+            yFinal = yOne + sind(-recoilAngle) * recoilAmount;
+        end
+    end
+    
+    % Plots recoiled shot location
+     plot(xFinal, yFinal, '*');
